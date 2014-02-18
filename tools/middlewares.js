dcdda8c1e93240fa6ac682cdb4e6168652e2d049
elodiebatista <elodie.batista@live.fr>
2014-02-18 15:32:08 +0100

    Allowing DELETE methods

--- tools/middlewares.js
+++ tools/middlewares.js
@@ -11,6 +11,7 @@ module.exports = {
 
 		res.header("Access-Control-Allow-Origin", "*");
 		res.header("Access-Control-Allow-Headers", "X-Requested-With, X-Cub-AuthToken, Content-Type");
+		res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 
 		next();
 	},
