31da3c831a327e077a95e25df50cd312b19d7b21
elodiebatista <elodie.batista@live.fr>
2014-03-01 14:21:07 +0100

    Fixing access control headers error

--- app.js
+++ app.js
@@ -21,6 +21,15 @@ app.set('view options', {
 });
 app.use(express.static(__dirname + '/public'));
 app.use(express.bodyParser());
+
+app.use(function (req, res, next) {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers", "X-Requested-With, X-Cub-AuthToken, Content-Type");
+ res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+ next();
+});
+
 app.use(app.router);
 app.use(express.favicon());
 app.use(express.logger('dev'));
