770a114f31c60073459b1964b5eb6f79b164af15
Kevin Lacabane <klacabane@gmail.com>
2014-03-16 16:57:22 +0100

    Adding fake consumption data

--- models/UserPlan.js
+++ models/UserPlan.js
@@ -6,7 +6,12 @@ var userPlanSchema = new mongoose.Schema({
 	user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
 	plan: { type: mongoose.Schema.Types.ObjectId, ref: 'Plan'},
 	billingDate: { type: Date, default: Date.now },
-	active: { type: Boolean, default: true }
+	active: { type: Boolean, default: true },
+    usage: {
+        storage: {type: Number, default: 430},
+        share: {type: Number, default: 311},
+        bandwidth: {type: Number, default: 500}
+    }
 });
 
 module.exports = mongoose.model('UserPlan', userPlanSchema);
